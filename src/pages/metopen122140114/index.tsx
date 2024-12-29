f2ddd7aef3d2fe29aa1dee6b6bb6d9ed178d4a38
padukadafa <padukadafa@gmail.com>
2024-12-30 04:53:08 +0700

    update: redirect_url

--- src/pages/metopen122140114/index.tsx
+++ src/pages/metopen122140114/index.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 function Metopen122140114() {
-  const redirect_url = "https://youtube.com";
+  const redirect_url = "https://www.youtube.com/watch?v=u8PRgxGfo38";
   const { push } = useRouter();
   useEffect(() => {
     push(redirect_url);
