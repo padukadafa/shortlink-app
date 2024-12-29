b2cd0ca85b50bde43cd1f5c2bf36e9fc0f4daf30
padukadafa <padukadafa@gmail.com>
2024-12-29 13:07:14 +0700

    feat: mongodb log

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const MONGODB_URL = process.env.MONGODB_URL || "";
 
 if (!MONGODB_URL) {
+  console.log(MONGODB_URL);
   throw new Error(
     `Please define \`MONGODB_URL\` environment variable inside ${
       process.env.NODE_ENV === "production" ? ".env" : ".env.development"
@@ -16,6 +17,9 @@ async function connectDB() {
   try {
     await mongoose.connect(MONGODB_URL);
   } catch (error) {
+    console.log(MONGODB_URL);
+    console.log(error);
+
     throw new Error(error as any);
   }
 }
